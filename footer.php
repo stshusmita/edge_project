2972a6bdd8b59c320c0787134d15eef19b34d74a
stshusmita <stahasilder19.eng@bu.ac.bd>
2024-06-08 22:19:56 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+			Developed and maintained by _Shusmita Tahasilder_. &copy; 2024
+		</div>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>E Home</title>
+	<link rel="stylesheet" href="style2.css">
+	
+	</head>
+	<body>
+
+		<div class="header">
+		
+		
+		  <h1 >
+Seri's Choise: Organic Beauty Products </h1>
+		  <p>..... Believe in Your Choise..... </p>
+	
+		 
+		  
+		</div>
+
+		<div class="topnav">
+		
+		  <a href="index.php">Home</a>
+		  <a href="courses.php"> Products List</a>
+		  <a href="application.php">Add Products</a>
+		  <a href="order.php">Buy Products</a>
+		  <marquee> Buy and Sell Organic Products. Contact: Email: www.stshusmita23@gmail.com, Phone: +880181667664, +8801915675787. Office Location: 90/B, Hatembagh, Dhanmondi-15, Dhaka-1209. 
+		  </marquee>
+		</div>
+		
+		
+		
+		
